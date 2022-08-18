36ec48c14009e691672b05b69978ee046ed7f6a8
MariaGadu <mariagadu@MariaGadu.maravilha>
2022-08-18 14:14:43 -0300

    creating project with buefy

---
+++ vue-ts/src/views/HomeView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="home">
+    <b-sidebar
+      type="is-light"
+      :fullheight="fullheight"
+      :fullwidth="fullwidth"
+      :overlay="overlay"
+      :right="right"
+      v-model="open"
+    >
+      <MenuBar />
+    </b-sidebar>
+    <div class="container">
+
+        <img alt="Vue logo" src="../assets/logo.png">
+        <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import MenuBar from '@/components/MenuBar.vue'
+
+@Component({
+  components: {
+    HelloWorld,
+    MenuBar
+  }
+})
+export default class HomeView extends Vue {
+  data () {
+    return {
+      open: true,
+      overlay: false,
+      fullheight: true,
+      fullwidth: false,
+      right: false
+    }
+  }
+}
+</script>
