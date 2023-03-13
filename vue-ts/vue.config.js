acb0bb605cd5543af4f252b6f23a2a172344d2f7
Felipe <felipemelz@gmail.com>
2023-03-13 19:17:13 -0300

    adding hints

--- vue-ts/vue.config.js
+++ vue-ts/vue.config.js
@@ -3,3 +3,12 @@ module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === "production" ? "/greenvalley/" : "/"
 })
+
+// first branch creation:
+// on main branch:
+// git subtree pull --prefix vue-ts/dist origin gh-pages
+
+// following updates:
+// on main branch:
+// git subtree push --prefix vue-ts/dist/ origin gh-pages
+
